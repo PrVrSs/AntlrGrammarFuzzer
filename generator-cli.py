b732d4fd06c003b993143dd7969a66ecdc84510b
sergey <resh.sersh@gmail.com>
2018-01-05 20:05:28 +0700

    fix default value

--- generator-cli.py
+++ generator-cli.py
@@ -3,16 +3,13 @@ import click
 
 
 @click.command()
-@click.option('--config_file', default=None, help='config_file.')
+@click.option('--config_file', default='config.ini', help='config_file.')
 def main(config_file):
-    config_file = 'config.ini'
     if config_file:
         try:
             ConfigGenerator(config_file=config_file)
         except ConfigFileError as e:
             print(e)
-    else:
-        pass
 
 
 if __name__ == "__main__":
