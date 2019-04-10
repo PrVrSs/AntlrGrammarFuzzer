fc7c91a39265292fbbd3d4bd978dde7ee19ca01b
Sergey Reshetnikov <resh.sersh@gmail.com>
2019-04-11 03:57:11 +0700

    add typing for ast and expressions

--- agf/antlr4_parser/ast.py
+++ agf/antlr4_parser/ast.py
@@ -1,7 +1,12 @@
 """AST"""
 import random
+import copy
+from typing import (
+    List,
+    Iterator
+)
 
-from .expressions import Literal
+from .expressions import Literal, Expression, Choice
 
 
 class Ast:
@@ -16,17 +21,18 @@ class Ast:
         '_default_max_or'
     )
 
-    def __init__(self, tree_: list = None, multiplication_scale: int = 1, random_scale: int = 1) -> None:
-        self.tree = tree_
-        self._multiplication_scale = multiplication_scale
-        self._random_scale = random_scale
-        self._default_min_zero = 0
-        self._default_min_one = 1
-        self._default_min_or = 0
-        self._default_max_or = 4
+    def __init__(self, tree_: List[Expression] = None, multiplication_scale: int = 1, random_scale: int = 1) -> None:
+        self.tree: List[Expression] = tree_ or []
 
-    def get_ast(self) -> list:
-        new_tree = self.tree[:]
+        self._multiplication_scale: int = multiplication_scale
+        self._random_scale: int = random_scale
+        self._default_min_zero: int = 0
+        self._default_min_one: int = 1
+        self._default_min_or: int = 0
+        self._default_max_or: int = 4
+
+    def get_ast(self) -> List[Expression]:
+        new_tree: List[Expression] = copy.copy(self.tree)
         while True:
             for expr in self.tree:
                 value = expr.accept(self)
@@ -39,75 +45,61 @@ class Ast:
                     break
             else:
                 break
-            self.tree = new_tree[:]
+
+            self.tree = copy.copy(new_tree)
         return self.tree
 
-    def return_rule(self) -> list:
-        rule = []
-        for expr in self.tree:
-            rule.append(expr.value)
-        return rule
+    def return_rule(self) -> Iterator[Expression]:
+        return (expr.value for expr in self.tree)
 
-    def visit_star_multiplication_grouping(self, expr) -> list:
+    def visit_star_multiplication_grouping(self, expr: Expression) -> List[Expression]:
         return self._unpacking_multiplication_grouping(expr=expr, min_=self._default_min_zero, max_=self._multiplication_scale)
 
-    def visit_random_grouping(self, expr) -> list:
+    def visit_random_grouping(self, expr: Expression) -> List[Expression]:
         return self._unpacking_random(expr=expr)
 
-    def visit_grouping(self, expr) -> list:
+    def visit_grouping(self, expr: Expression) -> List[Expression]:
         return self._unpacking_multiplication_grouping(expr=expr, min_=self._default_min_one, max_=self._default_min_one)
 
-    def visit_plus_multiplication_grouping(self, expr) -> list:
+    def visit_plus_multiplication_grouping(self, expr: Expression) -> List[Expression]:
         return self._unpacking_multiplication_grouping(expr=expr, min_=self._default_min_one, max_=self._multiplication_scale)
 
-
-    @staticmethod
-    def visit_tilde(expr):
-        return Literal('\x33')
-
-    def visit_equal(self, expr):
+    def visit_equal(self, expr: Expression):
         return expr.value.accept(self)
 
-    def visit_choice(self, expr):
+    def visit_choice(self, expr: Choice):
         return self._do_choice(expr.left, expr.right)
 
-    def _unpacking_random(self, expr):
-        random.seed()
-        result = expr.value if random.randint(self._default_min_zero, self._random_scale) else []
-        return result
-
-    @staticmethod
-    def _unpacking_multiplication_grouping(expr, min_: int = 1, max_: int = 1) -> list:
-        grouping_value = []
-        random.seed()
-        repeat = random.randint(min_, max_)
-        for _ in range(repeat):
-            grouping_value += expr.value
-        return grouping_value
-
-    def _do_choice(self, left, right):
-        random.seed()
-        check_left = random.randint(self._default_min_or, self._default_max_or)
-        return left.accept(self) if check_left else right.accept(self)
-
-    @staticmethod
-    def visit_literal(expr):
-        return expr
-
-    def visit_random_literal(self, expr):
+    def visit_random_literal(self, expr: Expression):
         return self._unpacking_random(expr)
 
-    def visit_star_multiplication(self, expr):
+    def visit_star_multiplication(self, expr: Expression):
         return self._unpacking_multiplication(expr, min_=self._default_min_zero, max_=self._multiplication_scale)
 
-    def visit_plus_multiplication(self, expr):
+    def visit_plus_multiplication(self, expr: Expression):
         return self._unpacking_multiplication(expr, min_=self._default_min_one, max_=self._multiplication_scale)
 
     @staticmethod
-    def _unpacking_multiplication(expr, min_: int = 1, max_: int = 2):
-        literal_value = []
+    def visit_tilde(expr: Expression) -> Expression:
+        return Literal(value='\x33')
+
+    @staticmethod
+    def visit_literal(expr) -> Expression:
+        return expr
+
+    def _unpacking_random(self, expr: Expression) -> List[Expression]:
+        return expr.value if self._repeat(min_=self._default_min_zero, max_=self._random_scale) else []
+
+    def _unpacking_multiplication_grouping(self, expr: Expression, min_: int = 1, max_: int = 1) -> List[Expression]:
+        return [exp for _ in range(self._repeat(min_=min_, max_=max_)) for exp in expr.value]
+
+    def _unpacking_multiplication(self, expr: Expression, min_: int = 1, max_: int = 2) -> List[Expression]:
+        return [expr.value for _ in range(self._repeat(min_=min_, max_=max_))]
+
+    def _do_choice(self, left: Expression, right: Expression) -> Expression:
+        return left.accept(self) if self._repeat(min_=self._default_min_or, max_=self._default_max_or) else right.accept(self)
+
+    @staticmethod
+    def _repeat(min_: int = 1, max_: int = 1) -> int:
         random.seed()
-        repeat = random.randint(min_, max_)
-        for _ in range(repeat):
-            literal_value.append(expr.value)
-        return literal_value
+        return random.randint(min_, max_)
--- agf/antlr4_parser/expressions.py
+++ agf/antlr4_parser/expressions.py
@@ -1,118 +1,126 @@
 """Expressions"""
+import abc
+import attr
+from typing import Any
 
 
-class Expression:
-    """Base Expression"""
-    pass
+class Expression(metaclass=abc.ABCMeta):
+    """Abstract Expression"""
+    type: Any = None
+    value: Any = None
+
+    @abc.abstractmethod
+    def accept(self, visitor):
+        raise NotImplementedError()
 
 
+@attr.s(slots=True)
 class Choice(Expression):
     """Choice expression"""
-    def __init__(self, left, operator, right):
-        self.type = 'Choice'
-        self.left = left
-        self.operator = operator
-        self.right = right
+    left = attr.ib(type=Expression)
+    operator = attr.ib(type=Any)
+    right = attr.ib(type=Expression)
+    type: str = 'Choice'
 
     def accept(self, visitor):
         return visitor.visit_choice(self)
 
 
+@attr.s(slots=True)
 class Equal(Expression):
     """Equal expression"""
-    def __init__(self, value):
-        self.type = 'Equal'
-        self.value = value
+    value = attr.ib(type=Any)
+    type: str = 'Equal'
 
     def accept(self, visitor):
         return visitor.visit_equal(self)
 
 
+@attr.s(slots=True)
 class Literal(Expression):
     """Literal expression"""
-    def __init__(self, value):
-        self.type = 'Literal'
-        self.value = value
+    value = attr.ib(type=Any)
+    type: str = 'Literal'
 
     def accept(self, visitor):
         return visitor.visit_literal(self)
 
 
+@attr.s(slots=True)
 class RandomLiteral(Expression):
     """RandomLiteral expression"""
-    def __init__(self, value):
-        self.type = 'RandomLiteral'
-        self.value = value
+    value = attr.ib(type=Any)
+    type: str = 'RandomLiteral'
 
     def accept(self, visitor):
         return visitor.visit_random_literal(self)
 
 
+@attr.s(slots=True)
 class StarMultiplication(Expression):
     """StarMultiplication expression"""
-    def __init__(self, value):
-        self.type = 'Multiplication'
-        self.value = value
+    value = attr.ib(type=Any)
+    type: str = 'Multiplication'
 
     def accept(self, visitor):
         return visitor.visit_star_multiplication(self)
 
 
+@attr.s(slots=True)
 class PlusMultiplication(Expression):
     """PlusMultiplication expression"""
-    def __init__(self, value):
-        self.type = 'Multiplication'
-        self.value = value
+    value = attr.ib(type=Any)
+    type: str = 'Multiplication'
 
     def accept(self, visitor):
         return visitor.visit_plus_multiplication(self)
 
 
+@attr.s(slots=True)
 class Grouping(Expression):
     """Grouping expression"""
-    def __init__(self, value):
-        self.type = 'Grouping'
-        self.value = value
+    value = attr.ib(type=Any)
+    type: str = 'Grouping'
 
     def accept(self, visitor):
         return visitor.visit_grouping(self)
 
 
+@attr.s(slots=True)
 class StarMultiplicationGrouping(Expression):
     """StarMultiplicationGrouping expression"""
-    def __init__(self, value):
-        self.type = 'MultiplicationGrouping'
-        self.value = value
+    value = attr.ib(type=Any)
+    type: str = 'MultiplicationGrouping'
 
     def accept(self, visitor):
         return visitor.visit_star_multiplication_grouping(self)
 
 
+@attr.s(slots=True)
 class PlusMultiplicationGrouping(Expression):
     """PlusMultiplicationGrouping expression"""
-    def __init__(self, value):
-        self.type = 'PlusMultiplicationGrouping'
-        self.value = value
+    value = attr.ib(type=Any)
+    type: str = 'PlusMultiplicationGrouping'
 
     def accept(self, visitor):
         return visitor.visit_plus_multiplication_grouping(self)
 
 
+@attr.s(slots=True)
 class RandomGrouping(Expression):
     """RandomGrouping expression"""
-    def __init__(self, value):
-        self.type = 'RandomGrouping'
-        self.value = value
+    value = attr.ib(type=Any)
+    type: str = 'RandomGrouping'
 
     def accept(self, visitor):
         return visitor.visit_random_grouping(self)
 
 
+@attr.s(slots=True)
 class Tilde(Expression):
     """Tilde expression"""
-    def __init__(self, value):
-        self.type = 'Tilde'
-        self.value = value
+    value = attr.ib(type=Any)
+    type: str = 'Tilde'
 
     def accept(self, visitor):
         return visitor.visit_tilde(self)
